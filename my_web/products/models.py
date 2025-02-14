6c1710f72dc7eae35d79a867fb938dc82d48512a
VCT18 <congtuyenproductions@gmail.com>
2025-02-14 17:20:44 +0700

    Add Brand

--- my_web/products/models.py
+++ my_web/products/models.py
@@ -9,9 +9,18 @@ class Category (models.Model):
 
     def __str__(self):
          return self.name
-    
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        ordering = ('name',)
+    def __str__(self):
+        return self.name
+
+     
 class Product(models.Model):
         category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) 
+        brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
         name = models.CharField(max_length=100)
         image = models.ImageField(upload_to='products/')
         price = models. DecimalField(max_digits=10, decimal_places=2)
